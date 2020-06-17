523875e8a88bc2e6e7465739c0d200cca7c90a9e
NasimKabir <mdkabir334@gmail.com>
2020-06-17 08:58:32 +0600

    working with product controller

--- src/main/java/com/nasim/controller/ProductController.java
+++ src/main/java/com/nasim/controller/ProductController.java
@@ -1,10 +1,24 @@
 package com.nasim.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nasim.repository.CategoryRepository;
+import com.nasim.repository.ProductRepository;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
+	@Autowired
+	private ProductRepository productRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
+	@PostMapping("/products")
+	public ResponseEntity<?> addNewProduct() {
+
+	}
 }
